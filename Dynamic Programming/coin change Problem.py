b9ba3f2de5d4b95ee2bfcedf89068f1675e23041
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-04-18 11:54:56 +0530

    Update coin change Problem.py

--- Dynamic Programming/coin change Problem.py
+++ Dynamic Programming/coin change Problem.py
@@ -1,5 +1,50 @@
-#User function Template for python3
+def count(S, m, n):
 
+    table = [[0 for x in range(m)] for x in range(n+1)]
+ 
+    for i in range(m):
+        table[0][i] = 1
+ 
+    for i in range(1, n+1):
+        for j in range(m):
+ 
+            x = table[i - S[j]][j] if i-S[j] >= 0 else 0
+ 
+            y = table[i][j-1] if j >= 1 else 0
+ 
+            table[i][j] = x + y
+ 
+    return table[n][m-1]
+ 
+arr = [1, 2, 3]
+m = len(arr)
+n = 4
+print(count(arr, m, n))
+
+#========================================================================
+
+# Dynamic Programming Python implementation of Coin
+
+def count(S, m, n):
+	table = [0 for k in range(n+1)]
+
+	table[0] = 1
+
+	for i in range(0,m):
+		for j in range(S[i],n+1):
+			table[j] += table[j-S[i]]
+
+	return table[n]
+
+
+arr = [1, 2, 3]
+m = len(arr)
+n = 4
+x = count(arr, m, n)
+print (x)
+
+
+#------------------------------------------------------------------
 class Solution:
     def __init__(self):
         self.dp = {}
