e3684026b08f89bbd29e2b69fb4bc1424f9f5408
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-04-27 11:16:49 +0530

    DP

---
+++ Dynamic Programming/Largest square formed in a matrix.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+    public:
+    int maxSquare(int n, int m, vector<vector<int>> mat)
+    {
+        int dp[n][m];
+        
+        int res = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(i == 0 or j == 0)
+                {
+                    dp[i][j] = mat[i][j];
+                }
+                else if(mat[i][j] == 1)
+                {
+                    dp[i][j] = 1 + min(
+                                            dp[i - 1][j - 1],
+                                            min(dp[i - 1][j], dp[i][j - 1])
+                                    );
+                }
+                else
+                {
+                    dp[i][j] = 0;
+                }
+                
+                res = max(res, dp[i][j]);
+            }
+        }
+        
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<int>> mat(n, vector<int>(m, 0));
+        for(int i = 0;i < n*m;i++)
+            cin>>mat[i/m][i%m];
+        
+        Solution ob;
+        cout<<ob.maxSquare(n, m, mat)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
