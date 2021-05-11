1c0c4c7770f8029a286d8c2237cea09b95171108
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-05-11 17:50:13 +0530

    Update Coin game winner where every player has three choices.py

--- Dynamic Programming/Coin game winner where every player has three choices.py
+++ Dynamic Programming/Coin game winner where every player has three choices.py
@@ -1,5 +1,7 @@
 #!bin/python3 
 
+# ------------------ Recursion ----------------------------------------------
+
 def util(n,x,y,st):
     if n < 0:
         return False
@@ -12,6 +14,28 @@ def util(n,x,y,st):
 def isAwinOrnot(n,x,y):
     return True if util(n,x,y,"A") else False
 
+
+#----------------------DP---------------------------------------------------
+#Memoization
+
+#!bin/python3 
+
+dp = [-1 for _ in range(100)]
+
+def util(n,x,y,st):
+    if n < 0:
+        return 0
+    if dp[n] != -1:
+        return dp[n]
+    newSt = "A" if st == "B" else "B"
+    if newSt == "A" and n == 0:
+        return 1
+    dp[n] = util(n-1,x,y,newSt) or util(n-x,x,y,newSt) or util(n-y,x,y,newSt)
+    return dp[n]
+
+def isAwinOrnot(n,x,y):
+    return True if util(n,x,y,"A") else False
+#---------------------------- use Functions by your need they all algo function have same name 000---------------------------
 
 if __name__ == "__main__":
     n = 2
