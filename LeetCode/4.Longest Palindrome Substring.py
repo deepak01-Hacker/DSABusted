d5ce30702d30c37846826b488c2447d8028de964
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-06-11 14:23:59 +0530

    DP ;)

--- LeetCode/4.Longest Palindrome Substring.py
+++ LeetCode/4.Longest Palindrome Substring.py
@@ -1,28 +1,45 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        if len(s) == 1:
+            return s
+        
+        if s == "":
+            return ""
+        
         n = len(s)
 
         dp = [[0 for _ in range(len(s)+1)] for _ in range(len(s)+1)]
 
-        ans = 0
+        ans = 1
+
+        startIndex = 0
 
         for i in range(1,len(s)+1):
             dp[i][i] = 1
+
+        for i in range(n-1):
+            if s[i] == s[i+1]:
+                ans = 2
+                startIndex = i
+                dp[i][i+1] = 2
+
+
 
         #print(n)
         for diff in range(2,n+1):
             for i in range(n-diff+1):
                 j = i+diff-1
 
-                #print(s[i],s[j])          it helps to think broad
-                if s[i] == s[j]:
-                    print(True)
+
+                if s[i] == s[j] and dp[i+1][j-1] != 0:
                     if diff == 2:
                         dp[i][j] = 2
                     else:
                         dp[i][j] = dp[i+1][j-1]+2
-
-                ans = max(ans,dp[i][j])
+                        if ans < dp[i][j]:
+                            ans = dp[i][j]
+                            startIndex = i
 
         #print(dp)
-        return ans
+
+        return s[startIndex:startIndex+ans]
