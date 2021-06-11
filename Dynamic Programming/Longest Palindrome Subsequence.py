98edb60aeef646fde8e7aa51b2dddb0646a02454
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-06-11 13:43:38 +0530

    DP

--- Dynamic Programming/Longest Palindrome Subsequence.py
+++ Dynamic Programming/Longest Palindrome Subsequence.py
@@ -1,3 +1,37 @@
+
+
+
+#-----------------------------------------------
+
+def lpSubstring(s):
+
+    n = len(s)
+
+    dp = [[0 for _ in range(len(s)+1)] for _ in range(len(s)+1)]
+    
+    for i in range(1,len(s)+1):
+        dp[i][i] = 1
+    
+    for diff in range(2,n+1):
+        for i in range(n-diff+1):
+            j = i+diff-1
+
+            if s[i] == s[j]:
+                if diff == 2:
+                    dp[i][j] = 2
+                    continue
+                dp[i][j] = dp[i+1][j-1]+2
+            else:
+                dp[i][j] = max(dp[i+1][j],dp[i][j-1])
+
+    return dp[0][n-1]
+
+
+
+
+
+#------------------------- Work for some cases ------------------------
+
 import sys
 
 #bin/python3!
