e4cb1cc6a97d52b1ca177cb2607634ef6f6c04ff
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-08-18 09:53:15 +0530

    Two Pointer

--- LeetCode/88. Merge Sorted Array.py
+++ LeetCode/88. Merge Sorted Array.py
@@ -1,3 +1,36 @@
+#----------------o(n)- -----------------
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        left = m-1
+        right = n-1
+
+        i = (m+n)-1
+
+        while(left >= 0 and right >= 0):
+            if nums1[left] > nums2[right]:
+                nums1[i] = nums1[left]
+                left -= 1
+            else:
+                nums1[i] = nums2[right]
+                right -= 1
+            i -= 1
+
+        while(left >= 0):
+            nums1[i] = nums1[left]
+            left -= 1
+            i -= 1
+
+        while(right >= 0):
+            nums1[i] = nums2[right]
+            right -= 1
+            i -= 1
+
+
+
+
+#------------- n(logn) ------------------
+
 import heapq as hp
 
 class Solution(object):
