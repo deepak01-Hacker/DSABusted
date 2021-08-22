94955b46377db8fad1203a9d9717297f68ecf2b4
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-08-22 16:18:31 +0530

    BIT

--- LeetCode/89. Gray Code.py
+++ LeetCode/89. Gray Code.py
@@ -26,4 +26,50 @@ class Solution(object):
         for j in range(len(res)):
             res[j] = int(res[j],2)
         return res
+    
+    #____________________________GFG ___________________ SAME APPROACH BUT EFFICIENT CODE ________________________
+    
+    # Python3 program to generate
+# n-bit Gray codes
+ 
+# This function generates all n
+# bit Gray codes and prints the
+# generated codes
+def generateGray(n):
+     
+    # Base case
+    if (n <= 0):
+        return ["0"]
+    if (n == 1):
+        return [ "0", "1" ]
+ 
+    # Recursive case
+    recAns = generateGray(n - 1)
+ 
+    mainAns = []
+     
+    # Append 0 to the first half
+    for i in range(len(recAns)):
+        s = recAns[i]
+        mainAns.append("0" + s)
+ 
+    # Append 1 to the second half
+    for i in range(len(recAns) - 1, -1, -1):
+        s = recAns[i]
+        mainAns.append("1" + s)
+ 
+    return mainAns
+ 
+# Function to generate the
+# Gray code of N bits
+def generateGrayarr(n):
+     
+    arr = generateGray(n)
+ 
+    # Print contents of arr
+    print(*arr, sep = "\n")
+ 
+# Driver Code
+generateGrayarr(3)
+ 
         
