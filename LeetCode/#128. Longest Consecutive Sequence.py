aaba864f1047846e7837a985777ed4f7d02ec1f2
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-10-02 13:06:34 +0530

    map

--- LeetCode/#128. Longest Consecutive Sequence.py
+++ LeetCode/#128. Longest Consecutive Sequence.py
@@ -5,6 +5,31 @@ class Solution(object):
         :rtype: int
         
         """
+        nums_set=set(nums)
+        max_consecutive_length=0
+        nums=list(nums_set)
+        
+        for i in range(len(nums)):
+            if nums[i]-1 in nums_set:
+                continue
+            current_length=1
+            temp=nums[i]
+            while(temp+1 in nums_set):
+                current_length+=1
+                temp+=1
+            max_consecutive_length=max(max_consecutive_length,current_length)
+        return max_consecutive_length
+    
+ #---------------------- Simple ---------------------------
+
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        
+        """
         length = 0
         prev = {}
         checker = {}
