83247dc9d273f19031df34bfa9da99317bfd0764
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2022-01-01 10:58:47 +0530

    DP

--- LeetCode/#312. Burst Balloons.py
+++ LeetCode/#312. Burst Balloons.py
@@ -1,3 +1,32 @@
+
+class Solution(object):
+
+    def maxCoins(self, nums):
+        
+        if (len(nums) == 0):
+            return 0;
+        nums.insert(0, 1);
+        nums.append(1);
+        n = len(nums)
+        l = 0
+        r = 0
+
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+
+        for k in range(2,n): # length
+            for l in range(0,n-k): 
+                r = l+k;
+                for i in range(l+1,r) :
+                    dp[l][r] = max(dp[l][r], nums[l]*nums[i]*nums[r]+dp[l][i]+dp[i][r]);
+
+
+
+        return dp[0][n-1];
+
+
+
+
+
 dp = {}
 class Solution(object):
     def boundHandle(self,i,nums):
